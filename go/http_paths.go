87181964d72fde078d830c8fca27c1c399a4132c
Nick Ryberg <nryberg@gmail.com>
2015-03-28 10:52:01 -0500

    Tweaks and fixes

--- go/http_paths.go
+++ go/http_paths.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"html/template"
 	"log"
@@ -37,6 +38,42 @@ func (s *Table_Data) ServeHTTP(
 }
 */
 
+func csvSplit(line string) []string {
+	working := strings.Split(line, ",")
+	pinpoint := 0
+	inquotes := false
+	for index, value := range working {
+		if inquotes == false {
+			if len(value) > 0 && value[0] == '\u0022' { // leading quote
+				working[index] = value[1:]
+				inquotes = true
+				pinpoint = index
+			}
+		} else {
+			if len(value) > 0 && value[len(value)-1] == '\u0022' { // trailing quote
+				working[pinpoint] += "," + value[:len(value)-1] // + working[index + 1]
+				working = append(working[:index], working[index+1:]...)
+				pinpoint = 0
+				inquotes = false
+			}
+			/*   Tweaking the array mid process screws up the index badly TODO  Fix re-indexing */
+			if len(value) > 0 && pinpoint > 0 && strings.ContainsRune(value, '\u0022') == false {
+				fmt.Printf("working:%v\n", working[index-3:index+4])
+				fmt.Printf("Value :%v\n", value)
+				fmt.Printf("Index : %v\n", index)
+				working[pinpoint] += "," + value
+				if (index + 1) > len(working) {
+					// working = append(working[:index], working[index+1:]...)
+				}
+			}
+		}
+
+	}
+	/*
+		}
+	*/
+	return working
+}
 func viewdata(w http.ResponseWriter, r *http.Request) {
 	td := new(Table_Data)
 
@@ -60,22 +97,33 @@ func loadData(td *Table_Data) {
 		os.Exit(1)
 	}
 
-	s := bufio.NewScanner(f)
-	counter := 0 // THERE HAS TO BE A BETTER WAY  TODO: Implement grab first line better
+	r := bufio.NewReader(f)
+	// counter := 0 // THERE HAS TO BE A BETTER WAY  TODO: Implement grab first line better
 
 	var records []Record
 	var record Record
 
-	for s.Scan() {
-		if counter == 0 {
-			td.Headers = strings.Split(s.Text(), ",")
-		} else {
+	rcsv := csv.NewReader(r)
+	headers, err := rcsv.Read()
+	td.Headers = headers
+	values, err := rcsv.Read()
+	record.Values = values
+	records = append(records, record)
+	fmt.Printf("Recsample %v\n", record)
+	//td.Records[0].Values = record
+	/*
+		for s.Scan() {
+			if counter == 0 {
 
-			record.Values = strings.Split(s.Text(), ",")
-			records = append(records, record)
+				td.Headers = csvSplit(s.Text())
+			} else {
+
+				record.Values = csvSplit(s.Text())
+				records = append(records, record)
+			}
+			counter += 1
 		}
-		counter += 1
-	}
+	*/
 
 	td.Records = records
 }
